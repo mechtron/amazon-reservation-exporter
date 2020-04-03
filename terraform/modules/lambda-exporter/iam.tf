69f9ef70daabbc9cce42a67366b724cda0d88f30
Corey Gale <corey@gumgum.com>
2020-04-02 21:59:54 -0700

    Test hard-coding var

--- terraform/modules/lambda-exporter/iam.tf
+++ terraform/modules/lambda-exporter/iam.tf
@@ -63,7 +63,7 @@ resource "aws_iam_role_policy" "app_permissions" {
             "Action": [
               "sts:AssumeRole"
             ],
-            "Resource": "${var.assume_role_arns}"
+            "Resource": ["arn:aws:iam::564061425806:role/DevOpsAdminAccess", "arn:aws:iam::000173938423:role/DevOpsAdminAccess", "arn:aws:iam::634280838161:role/DevOpsAdminAccess", "arn:aws:iam::797146828779:role/DevOpsAdminAccess"]
         }
     ]
 }
